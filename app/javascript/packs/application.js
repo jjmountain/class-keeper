5c5450ca878f6c9c39f29c34a604ee2ecf0a2d50
James Devereux <devereuxjj@gmail.com>
2020-02-03 22:46:57 +0900

    added sb admin pro theme and it works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,11 @@ import "controllers"
 
 import "../plugins/flatpickr"
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'assets/javascripts/scripts.js'
+import 'assets/stylesheets/styles.css'
+
+import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/css/all.css'
