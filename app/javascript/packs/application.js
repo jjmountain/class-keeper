85b8c7e06be3909366e11639acde3f3e8ef414ed
James Devereux <devereuxjj@gmail.com>
2020-03-08 00:39:33 +0900

    deleted js in assets pipeline and about to setup action cable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,3 +33,41 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// copied from application.js in assets pipeline
+
+$(function () {
+  $(document).on('change', '.remote-select', function (e) {
+    var url = $(this).find(':selected').attr('data-url')
+    var target = $(this).attr('data-target')
+
+    if (url) {
+      $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (json) {
+          var target_select = $(target)
+          console.log('target select:', target_select)
+          target_select.empty()
+          json.map(function (item) {
+            value = $('<option></option>').attr('value', item.id).text(item.name)
+            console.log('Item:', item, 'value:', value)
+            target_select.append(value)
+          })
+        },
+        error: function () {}
+      })
+    }
+  })
+})
+
+// $(document).ready(function() {
+//   $("#dttb").dataTable(
+//     {
+//       "lengthMenu": [[10, 25, -1], [10, 25, "All"]],
+//       "info": false,
+//       scrollCollapse: true,
+      
+//     }
+//   );
+// });
